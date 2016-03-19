ef5e54ea93b0e7a728367b111997c6270ff641ed
Tobias Kiertscher <dev@mastersign.de>
2016-03-19 18:06:32 +0100

    initialize project structure

---
+++ README.md
@@ -0,0 +1,14 @@
+# hark
+
+> A Clojure library for parsing an `OutputStream` for separated strings.
+
+## Usage
+
+FIXME: explanation
+
+## License
+
+Copyright © 2016 Tobias Kiertscher <dev@mastersign.de>
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject hark "0.1.0-SNAPSHOT"
+  :description "A Clojure library for parsing an OutputStream for separated strings."
+  :url "http://github.com/mastersign/hark"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[junit/junit "4.11"]]
+                   :java-source-paths ["src/java" "test/java"]}}
+  :plugins [[lein-junit "1.1.8"]]
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  :junit ["test/java"])
