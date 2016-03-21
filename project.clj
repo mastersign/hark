7db3ec867be8ff1ccd28d511fab5ea6065680f4f
Tobias Kiertscher <tobias.kiertscher@de.bosch.com>
2016-03-21 14:40:07 +0100

    added codox for generating the API docs for Clojure

--- project.clj
+++ project.clj
@@ -5,10 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[junit/junit "4.11"]]
-                   :java-source-paths ["src/java" "test/java"]}}
-  :plugins [[lein-junit "1.1.8"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :java-source-paths ["src/java"]
-  :junit ["test/java"])
+  :profiles {:dev {:dependencies [[junit/junit "4.11"]]
+                   :java-source-paths ["src/java" "test/java"]}}
+  :plugins [[lein-junit "1.1.8"]
+            [lein-codox "0.9.4"]]
+  :junit ["test/java"]
+  :codox {:output-path "doc/clj"
+          :source-uri "https://github.com/mastersign/hark/blob/{version}/{filepath}#{line}"})
